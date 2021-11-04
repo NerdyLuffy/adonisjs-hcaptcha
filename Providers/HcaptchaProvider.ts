bf6bf29e23f604da2cb192f06dbc36728d7c1316
Yash K <yashsk@protonmail.com>
2021-11-05 00:08:15 +1100

    feat: Added provider for Hcaptcha

---
+++ Providers/HcaptchaProvider.ts
@@ -0,0 +1,25 @@
+import { ApplicationContract } from '@ioc:Adonis/Core/Application'
+import { HcaptchaConfig } from '@ioc:Hcaptcha'
+import HcaptchaValidator from '../src/Hcaptcha'
+
+export default class MongoProvider {
+  public static needsApplication = true
+
+  constructor(protected application: ApplicationContract) {
+    this.application.container.singleton('Hcaptcha', () => {
+      const config: HcaptchaConfig = this.application.container
+        .use('Adonis/Core/Config')
+        .get('hcaptcha', {})
+
+      return new HcaptchaValidator(config)
+    })
+  }
+
+  public register() {}
+
+  public async boot() {}
+
+  public async ready() {}
+
+  public async shutdown() {}
+}
